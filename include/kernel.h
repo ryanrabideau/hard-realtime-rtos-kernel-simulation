2817c589964c939887b729a6049afa2a7c82e0c3
Ryan Rabideau <ryanfrabideau@gmail.com>
2026-09-07 12:55:13 -0400

    Add watchdog functionality and update task_create signature

--- include/kernel.h
+++ include/kernel.h
@@ -6,6 +6,7 @@
 
 #define TASK_STACK_SIZE 512
 #define STACK_CANARY 0xA5A5A5A5
+#define WATCHDOG_TIMEOUT_TICKS 100
 
 typedef enum {
     TASK_READY,
@@ -22,17 +23,23 @@ typedef struct tcb {
     uint8_t           priority;
     task_state_t      state;
     bool              ready;
+
     uint32_t          period_ticks;
     uint32_t          deadline_ticks;
     uint32_t          wcet_ticks;
+
     uint32_t          last_release;
     uint32_t          runtime;
+
     task_entry_t      entry;
+
     uint8_t           stack[TASK_STACK_SIZE];
     uint32_t          stack_pointer;
     uint32_t          stack_size_used;
+
     int               app_state;
     int               app_sub_state;
+
     uint32_t          release_time;
     uint32_t          deadline_time;
     bool              deadline_miss;
@@ -51,18 +58,31 @@ extern tcb_t          tasks[10];
 extern uint32_t       num_tasks;
 extern uint32_t       watchdog_counter;
 
+extern mutex_t shared_mutex;
+
 void kernel_init(void);
 void kernel_tick(void);
+
 void task_yield(void);
 tcb_t* scheduler_select_next(void);
-void task_create(uint32_t id, uint8_t priority, task_entry_t entry);
+
+void task_create(uint32_t id,
+                 uint8_t priority,
+                 task_entry_t entry);
+
 void task_release(uint32_t task_id);
+
 void task_init_stack(tcb_t* t);
 bool task_check_stack_overflow(tcb_t* t);
+
 void task_block(tcb_t* t);
 void task_unblock(tcb_t* t);
+
 void task_monitor_deadlines(void);
+
 void watchdog_check(void);
+void watchdog_kick(void);
+
 void mutex_init(mutex_t* m);
 void mutex_lock(mutex_t* m);
 void mutex_unlock(mutex_t* m);
@@ -70,7 +90,5 @@ void mutex_unlock(mutex_t* m);
 extern void task1_high(void);
 extern void task2_medium(void);
 extern void task3_low(void);
-
-extern mutex_t shared_mutex;
 
 #endif /* KERNEL_H */
